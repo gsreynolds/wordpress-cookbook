38149f4d842de9c543bda11436653e3988dacb44
Gavin Reynolds <g.reynolds@src.gla.ac.uk>
2017-08-22 16:34:20 +0100

    Disable configuring mysql here, Chef 13 resource cloning issues

--- recipes/database.rb
+++ recipes/database.rb
@@ -42,31 +42,31 @@ if is_local_host? db['host']
   # The following is required for the mysql community cookbook to work properly
   include_recipe 'selinux::disabled' if node['platform_family'] == 'rhel'
 
-  mysql_service db['instance_name'] do
-    port db['port']
-    version db['mysql_version']
-    initial_root_password db['root_password']
-    action [:create, :start]
-  end
+  # mysql_service db['instance_name'] do
+  #   port db['port']
+  #   version db['mysql_version']
+  #   initial_root_password db['root_password']
+  #   action [:create, :start]
+  # end
 
-  socket = "/var/run/mysql-#{db['instance_name']}/mysqld.sock"
-
-  if node['platform_family'] == 'debian'
-    link '/var/run/mysqld/mysqld.sock' do
-      to socket
-      not_if 'test -f /var/run/mysqld/mysqld.sock'
-    end
-  elsif node['platform_family'] == 'rhel'
-    link '/var/lib/mysql/mysql.sock' do
-      to socket
-      not_if 'test -f /var/lib/mysql/mysql.sock'
-    end
-  end
+  # socket = "/var/run/mysql-#{db['instance_name']}/mysqld.sock"
+  #
+  # if node['platform_family'] == 'debian'
+  #   link '/var/run/mysqld/mysqld.sock' do
+  #     to socket
+  #     not_if 'test -f /var/run/mysqld/mysqld.sock'
+  #   end
+  # elsif node['platform_family'] == 'rhel'
+  #   link '/var/lib/mysql/mysql.sock' do
+  #     to socket
+  #     not_if 'test -f /var/lib/mysql/mysql.sock'
+  #   end
+  # end
 
   mysql_connection_info = {
     :host     => 'localhost',
     :username => 'root',
-    :socket   => socket,
+    # :socket   => socket,
     :password => db['root_password']
   }
 
@@ -86,6 +86,8 @@ if is_local_host? db['host']
   mysql_database_user db['user'] do
     connection    mysql_connection_info
     database_name db['name']
+    password      db['pass']
+    host          db['host']
     privileges    [:all]
     action        :grant
   end
